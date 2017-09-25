3bff00b290f76132cc73c3ca0b59dabdbaafb3dd
Schezwan <schezwan.daum.net>
2017-09-25 22:49:04 +0900

    Clear

---
+++ 9498/main.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+	int s;
+	scanf("%d", &s);
+	if (90 <= s && s <= 100) printf("A");
+	else if (80 <= s && s < 90) printf("B");
+	else if (70 <= s && s < 80) printf("C");
+	else if (60 <= s && s < 70) printf("D");
+	else printf("F");
+}
