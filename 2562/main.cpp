1f94156e08313c00e0490a7f79b7d0a170b642ce
Schezwan <schezwan.daum.net>
2017-10-03 00:16:59 +0900

    Clear

--- 2562/main.cpp
+++ 2562/main.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 
 int main() {
-	printf("답은 이걸 보는 당신이 직접 구하세요");
+	int k, max = 0, p = 0;
+	for (int i = 0; i < 9; i++) {
+		scanf("%d", &k);
+		if (k > max) {
+			max = k;
+			p = i + 1;
+		}
+	}
+	printf("%d\n%d", max, p);
 }
